54ea1a837f38b6e45f2351f127f05eaabff04930
pedrogg20 <pedroeliasferreira@gmail.com>
2026-03-03 14:38:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // Garante que os caminhos sejam relativos para o GitHub Pages
+    base: '/curriculo/', // Garante que os caminhos sejam relativos para o GitHub Pages
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
